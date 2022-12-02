9349a9bff05e44e2c3cf763abd143aac0baa1fd5
GuiEpi <guillaume.coussot@epitech.eu>
2022-12-02 18:26:54 +0100

    day 1 done

---
+++ day1/calorie_counting.py
@@ -0,0 +1,22 @@
+# Part. 1
+total_calories = []
+count = 0
+
+with open('input.txt', 'r') as f:
+    for line in f:
+        if line != '\n':
+            count += int(line)
+        total_calories.append(count)
+        count = 0
+
+max_calories = max(total_calories)
+print(max_calories)
+
+# Part. 2
+top_three_elves = 0
+
+for i in range(3):
+    top_three_elves += max(total_calories)
+    total_calories.remove(max(total_calories))
+
+print(top_three_elves)
