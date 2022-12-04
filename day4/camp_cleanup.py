7854f074d9ed80ad391ef7635b078451545a2a9f
Guillaume <guillaume.coussot@hotmail.fr>
2022-12-04 19:58:39 +0100

    day 1 part 1 done

---
+++ day4/camp_cleanup.py
@@ -0,0 +1,14 @@
+#Part. 1
+pair = 0
+
+with open("input.txt", "r") as f:
+    for line in f:
+        elf1, elf2 = line.split(',')
+        elf1_section1, elf1_section2 = elf1.split('-')
+        elf2_section1, elf2_section2 = elf2.split('-')
+        if int(elf1_section1) >= int(elf2_section1) and int(elf1_section2) <= int(elf2_section2):
+            pair += 1
+        elif int(elf2_section1) >= int(elf1_section1) and int(elf2_section2) <= int(elf1_section2):
+            pair += 1
+print(pair)
+        
