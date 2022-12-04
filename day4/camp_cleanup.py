065d1890aef63915ef1bb1f40d9fcf9455018b90
Guillaume <guillaume.coussot@hotmail.fr>
2022-12-04 21:17:57 +0100

    day 3 done

--- day4/camp_cleanup.py
+++ day4/camp_cleanup.py
@@ -1,4 +1,4 @@
-#Part. 1
+# Part. 1
 pair = 0
 
 with open("input.txt", "r") as f:
@@ -12,3 +12,20 @@ with open("input.txt", "r") as f:
             pair += 1
 print(pair)
         
+# Part. 2
+overlaps = 0
+
+with open("input.txt", "r") as f:
+    for line in f:
+        elf1, elf2 = line.split(',')
+        elf1_section1, elf1_section2 = elf1.split('-')
+        elf2_section1, elf2_section2 = elf2.split('-')
+        if int(elf1_section1) <= int(elf2_section1) and int(elf1_section2) >= int(elf2_section1):
+            overlaps += 1
+        elif int(elf2_section1) <= int(elf1_section1) and int(elf2_section2) >= int(elf1_section1):
+            overlaps += 1
+print(overlaps)
+
+
+
+
