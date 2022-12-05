4a7849d437ffa834eeaf45490df23c0457013709
Guillaume <guillaume.coussot@hotmail.fr>
2022-12-06 00:49:59 +0100

    change method day3 -> dict to ascii

--- day3/rucksack_reorganization.py
+++ day3/rucksack_reorganization.py
@@ -1,57 +1,3 @@
-priorities = {
-    "a": 1,
-    "b": 2,
-    "c": 3,
-    "d": 4,
-    "e": 5,
-    "f": 6,
-    "g": 7,
-    "h": 8,
-    "i": 9,
-    "j": 10,
-    "k": 11,
-    "l": 12,
-    "m": 13,
-    "n": 14,
-    "o": 15,
-    "p": 16,
-    "q": 17,
-    "r": 18,
-    "s": 19,
-    "t": 20,
-    "u": 21,
-    "v": 22,
-    "w": 23,
-    "x": 24,
-    "y": 25,
-    "z": 26,
-    "A": 27,
-    "B": 28,
-    "C": 29,
-    "D": 30,
-    "E": 31,
-    "F": 32,
-    "G": 33,
-    "H": 34,
-    "I": 35,
-    "J": 36,
-    "K": 37,
-    "L": 38,
-    "M": 39,
-    "N": 40,
-    "O": 41,
-    "P": 42,
-    "Q": 43,
-    "R": 44,
-    "S": 45,
-    "T": 46,
-    "U": 47,
-    "V": 48,
-    "W": 49,
-    "X": 50,
-    "Y": 51,
-    "Z": 52,
-}
 # Part. 1
 sum = 0
 
@@ -73,7 +19,12 @@ with open("input.txt", "r") as f:
             if types in d:
                 common += types
                 break
-        sum += priorities[common]
+        ascii_ = ord(common)
+        if ascii_ > 96:
+            ascii_ -= 96 # a(char) = 97(dec) - 1(place in alphabet)
+        else:
+            ascii_ -= 38 # A(char) = 65(dec) - 27(place in alphabet)
+        sum += ascii_
 print(sum)
 
 # Part. 2
@@ -105,7 +56,12 @@ with open("input.txt", "r") as f:
                 if types in d1 and types in d2:
                     common += types
                     break
-            sum += priorities[common]
+            ascii_ = ord(common)
+            if ascii_ > 96:
+                ascii_ -= 96
+            else:
+                ascii_ -= 38
+            sum += ascii_
             d1.clear()
             d2.clear()
 print(sum)
